81448a7669a59fbf2a2be23d5bbc9c25c0cf5952
Collin Price <collin@collinprice.com>
2015-11-06 22:35:50 -0500

    testing pygame keyboard input

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("Hello Spider")
+#print ("Hello Spider")
+
+import pygame
+
+
+pygame.init()
+done = False
+
+while not done:
+	for event in pygame.event.get()
+		if event.type == pygame.KEYDOWN:
+			print event.key
+			
+
