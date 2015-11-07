0d48c1fe5393639f4cdfb9e54267fb25b61baa88
= <=>
2015-11-06 22:59:24 -0500

    new example

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import curses
-stdscr = curses.initscr()
-curses.cbreak()
-stdscr.keypad(1)
+import sys,tty,termios
+class _Getch:
+    def __call__(self):
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(3)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
 
-stdscr.addstr(0,10,"Hit 'q' to quit")
-stdscr.refresh()
+def get():
+        inkey = _Getch()
+        while(1):
+                k=inkey()
+                if k!='':break
+        if k=='\x1b[A':
+                print "up"
+        elif k=='\x1b[B':
+                print "down"
+        elif k=='\x1b[C':
+                print "right"
+        elif k=='\x1b[D':
+                print "left"
+        else:
+                print "not an arrow key!"
 
-key = ''
-while key != ord('q'):
-    key = stdscr.getch()
-    stdscr.addch(20,25,key)
-    stdscr.refresh()
-    if key == curses.KEY_UP: 
-        stdscr.addstr(2, 20, "Up")
-    elif key == curses.KEY_DOWN: 
-        stdscr.addstr(3, 20, "Down")
+def main():
+        for i in range(0,20):
+                get()
 
-curses.endwin()
+if __name__=='__main__':
+        main()
