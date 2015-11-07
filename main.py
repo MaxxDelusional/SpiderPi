e709add7d7b1781bab996b62c5c0b9ac35b06e02
Collin Price <collin@collinprice.com>
2015-11-06 23:31:19 -0500

    changed to keys.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys,tty,termios
+
 class _Getch:
     def __call__(self):
             fd = sys.stdin.fileno()
             old_settings = termios.tcgetattr(fd)
             try:
                 tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(3)
+                ch = sys.stdin.read(1)
             finally:
                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
             return ch
@@ -17,20 +18,31 @@ def get():
                 if k!='':break
 
         # print 'you pressed', ord(k)
-        if k=='\x1b[A':
-                print "up"
-        elif k=='\x1b[B':
-                print "down"
-        elif k=='\x1b[C':
-                print "right"
-        elif k=='\x1b[D':
-                print "left"
-        else:
-                print "not an arrow key!"
+        # if k=='\x1b[A':
+        #         print "up"
+        # elif k=='\x1b[B':
+        #         print "down"
+        # elif k=='\x1b[C':
+        #         print "right"
+        # elif k=='\x1b[D':
+        #         print "left"
+        # else:
+        #         print "not an arrow key!"
+        print k
+
+        if k == 'q':
+        	return True
+
+        # print k
+        return False
+
+
 
 def main():
         while True:
-                get()
+                v = get()
+                if v:
+                	break
 
 if __name__=='__main__':
         main()
